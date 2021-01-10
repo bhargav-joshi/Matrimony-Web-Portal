6780085274bdcbb58bfb62142a3634d911e59994
Bhargav Joshi <47782249+bhargav-joshi@users.noreply.github.com>
2021-01-10 17:16:20 +0530

    About Module added

---
+++ about.php
@@ -0,0 +1,238 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Vivah.com | About</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+ <?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="grid_3">
+  <div class="container">
+   <div class="breadcrumb1">
+     <ul>
+        <a href="index.php"><i class="fa fa-home home_1"></i></a>
+        <span class="divider">&nbsp;|&nbsp;</span>
+        <li class="current-page">About Us</li>
+     </ul>
+   </div>
+   <div class="about">
+   	  <div class="col-md-6 about_left">
+   	  	<img src="images/about1.svg" class="img-responsive" alt=""/>
+   	  </div>
+   	  <div class="col-md-6 about_right">
+   	  	<h1>About Vivah.com</h1>
+   	  	<p>Vivah.com is one of India’s leading matrimonial websites that has helped lakhs of members find their perfect life partner.</p>
+   	  	<div class="accordation">
+		   <div class="jb-accordion-wrapper">
+				<div class="jb-accordion-title">Important features of vivah.com:  <button type="button" class="jb-accordion-button" data-toggle="collapse" data-target="#accordion-1-"><i class="fa fa-angle-down"> </i></button></div>
+				<p><!-- /.accordion-title -->
+				</p><div id="accordion-1-" class="jb-accordion-content collapse in" style="height: auto;">
+				<p>•	Free and easy profile registration
+•	Safe and secure site. 100% privacy guaranteed
+•	registered profiles are manually screened and validated to ensure they meet the norms of our site
+•	User friendly interface and features for easy partner search
+•	Customer service team to assist you in all the queries raised
+•	Groups forum to have discussion on various topics and build relationships online 
+</p>
+				</div>
+				<p><!-- /.collapse --></p>
+			</div>
+			<div class="jb-accordion-wrapper">
+				<div class="jb-accordion-title">Our  Mission <button type="button" class="jb-accordion-button" data-toggle="collapse" data-target="#accordion2-"><i class="fa fa-angle-down"> </i></button></div>
+				<p><!-- /.accordion-title -->
+				</p><div id="accordion2-" class="jb-accordion-content collapse ">
+				<p>We believe choosing a life partner is a big and important decision, and hence work towards giving a simple and secure matchmaking experience for you and your family. Each profile registered with us goes through a manual screening process before going live on site; we provide superior privacy controls for Free; and also verify contact information of members.
+You can register for Free and search according to your specific criteria on age, height, community, profession, income, location and much more- on your computer, tablet or mobile. Regular custom mails and notifications make the process easier and take you closer to your Jeevansathi!
+ </p>
+				</div>
+				<p><!-- /.collapse --></p>
+			</div>
+			<div class="jb-accordion-wrapper">
+				<div class="jb-accordion-title">Our Portal<button type="button" class="jb-accordion-button" data-toggle="collapse" data-target="#accordion3"><i class="fa fa-angle-down"> </i></button></div>
+				<p><!-- /.accordion-title -->
+				</p><div id="accordion3" class="jb-accordion-content collapse ">
+				<p>vivah.com is the only portal in the history of online matrimony to provide matrimony services solely based on community. When you register on vivah.com, based on your community, your profile will be assigned to the relevant community matrimony site where you can search and contact profiles from your community. Empowered by innovative tools and modern technologies, vivah.com provides the widest choice of profiles that matches your criteria and expectations. <br>
+vivah.com is an Indian Matrimony website. Matches are made based on Both Traditional and Scientific Methodology. vivah.com remains dedicated to studying and understanding what makes long-term relationships successful by shepherding constant, scientific research to find a Companion-For-Life that perfectly blend with you and your ideologies and off-course your family too. vivah.com operates Domestic markets. 
+				 </p>
+				</div>
+				<p><!-- /.collapse --></p>
+			</div>
+		</div>
+   	  </div>
+   	  <div class="clearfix"> </div>
+   </div>
+  </div>
+</div>
+<div class="about_middle">
+	<div class="container">
+	  <h2>Happy Clients</h2>
+	  <div class="about_middle-grid1">
+		<div class="col-sm-6 testi_grid list-item-0">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/a1.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">I love the simple ui and easy functions.</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+		    <small class="testi-meta"><span class="user">Janhavi J</span></small>
+		</div>
+		<div class="col-sm-6 testi_grid list-item-1">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/a2.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">The Options like checking the partners with their id's and regular search is great.</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+			<small class="testi-meta1"><span class="user">Siddharth W</span></small>
+		</div>
+		<div class="clearfix"> </div>
+	  </div>
+	  <div class="about_middle-grid2">
+		<div class="col-sm-6 testi_grid list-item-0">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/a2.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">This platform gave me my true life partner....Thank you so much</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+		    <small class="testi-meta"><span class="user">Chinmay M</span></small>
+		</div>
+		<div class="col-sm-6 testi_grid list-item-1">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/a1.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">I like the Featured profiles section which shows the differnt good profiles at a time.</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+			<small class="testi-meta1"><span class="user">Suchita P</span></small>
+		</div>
+		<div class="clearfix"> </div>
+	  </div>
+	</div>
+</div>
+<div class="about_bottom" align="center">
+	<div class="container">
+	<center><img src="images/team.svg" class="img-responsive" alt=""/ width="300px" height="200px"><br></center>
+		<h3> Meet the Team</h3>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/bhargav.jpg"  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Mr.Bhargav Joshi</a></h4>
+			    	<p>CEO & Founder</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/Kartik.jpg"  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Kartik Rane </a></h4>
+			    	<p>Marketinng & Operations</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/Gaurav.jpg"  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Gaurav Mahind</a></h4>
+			    	<p>Support Department</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/Admin.png"  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Admin</a></h4>
+			    	<p>Administrator Work</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="clearfix"> </div>
+	</div>
+</div>
+
+
+<?php include_once("footer.php");?>
+
+</body>
+</html>	
