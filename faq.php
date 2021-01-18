632231f4eca8ab16abe121b1dbb4c3b28c7e4bd9
Bhargav Joshi <47782249+bhargav-joshi@users.noreply.github.com>
2021-01-18 18:15:31 +0530

    FAQs Section Added

---
+++ faq.php
@@ -0,0 +1,214 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Vivah.com | FAQs</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+<?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="grid_3">
+  <div class="container">
+   <div class="breadcrumb1" align="center">
+     <img src="images/faq.svg" class="img-responsive" alt=""/ width="300px" height="200px"> <br>
+     <ul>
+        <a href="index.php"><i class="fa fa-home home_1"></i></a>
+        <span class="divider">&nbsp;|&nbsp;</span>
+        <li class="current-page"><h1 align="center">Frequently Asked Questions (FAQs) </h1></li>
+        <hr>
+     </ul>
+   </div>
+  
+   <dl class="faq-list">
+   	<div>
+   	  <ul>    
+        <li><b>1. Registration <b></li>
+        	<hr>
+     </ul>
+    <br> 
+    </div>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to register?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker1">A.</h4>
+	<p class="m_4">Login to www.Vivah.com <br>
+	Here, you can choose to register as a free member.
+</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>What all registration details are to be filled?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">Provide name, age, gender, e-mail ID, and other details which you would fill in along with 4 photographs of your own. Set username and password according to the guidelines mentioned while filling in.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How will the profile be created?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">To connect with a user and express your interest you will first have to request Vivah.com.</p>
+	</dd>
+	<div>
+	   	  <ul>    
+        <li><b>2. Ways to Contact <b></li>
+        	<hr>
+     </ul>
+    <br> 
+	</div>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to connect with a profile that you are interested in?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">To connect with a user and express your interest you will first have to request Vivah.com.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to contact the user that you are interested in?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">After your request gets approved by Vivah.com, the user’s contact number and chat box will be revealed to you where you can either chat with the user and express your interest in them or call them directly.</p>
+	</dd>
+	</dd>
+	 <div>
+	   	  <ul>    
+        <li><b>3. Login Details & Profile Authenticity <b></li>
+        	<hr>
+     </ul>
+     </div>
+    <br> 
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to login?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">Login to www.Vivah.com and then fill in your username and password and login.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>What should you do in case of login error despite filling correct login details?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">You can contact us via e-mail at support@vivah.com and tell us about what difficulty you are facing and we will resolve it for you at the earliest.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to see other Vivah.com profiles?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">You will need to have an account with Vivah.com and login to see other profiles as well as other details related to those profiles.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to know if a profile is duplicate?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">We, at Vivah.com, try our best to serve you, your matrimonial requirement as safely and authentically as possible. We understand the frauds taking place on matrimonial sites and we strive to make our website, the safest. In doing this, we provide every user of ours, a unique ‘Matrimonial ID’ which cannot be copied into other profile/s and our system verifies every user before they are registered as users of Vivah.com.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to edit profile?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">You can change your profile details any time using the edit profile feature.</p>
+	</dd>
+	</dd>
+	<div>
+	   	  <ul>    
+        <li><b>4. Search Profile <b></li>
+        	<hr>
+     </ul>
+    <br> 
+	</div>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to search profiles at Vivah.com?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">You can use the search filter feature to see profiles based on your preferences. You can also search profiles by putting in their ‘Matrimonial IDs’</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to block and report a profile?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">You will need to contact us at support@vivah.com in order to report and block the user profile and provide details as to why you want to block the profile and report it.</p>
+	</dd>
+	<div>
+		   	  <ul>    
+        <li><b>5. Support <b></li>
+        	<hr>
+     </ul>
+    <br> 
+    </div>
+    <dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>Need help with using Vivah.com or share your feedback?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">We, at Vivah.com, aim to serve you the near perfect, seamless and safe matrimonial website experience. In case you want to report a problem, need help understanding Vivah.com features or share your feedback, you can always do that at support@vivah.com </p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>Feature request?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">Please contact us at support@vivah.com and help us serve you better.</p>
+	</dd>
+   </dl>
+  </div>
+</div>
+
+
+<?php include_once("footer.php");?>
+
+</body>
+</html>	
